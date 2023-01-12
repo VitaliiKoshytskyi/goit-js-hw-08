22905ba384d741d54d786c0807d85c3989f51219
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-01-12 18:04:40 +0200

    updated task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,11 +5,10 @@ import throttle from 'lodash.throttle';
     const player = new Player(iframe);
 
 const storedTime = localStorage.getItem("videoplayer-current-time")
-console.log(storedTime)
+
 player.setCurrentTime(JSON.parse(storedTime))
 const timeHandler = (event) => {
-    console.log('hey')
-    console.log(event)
+    
     localStorage.setItem("videoplayer-current-time", JSON.stringify(event.seconds))
     
 }
